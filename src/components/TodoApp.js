8014ccc706a06cd7af7a706c52266288178d5c5a
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-18 12:26:00 +0430

    add global state for todos in TodoApp

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import styles from './TodoApp.module.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 const TodoApp = () => {
+  const [todos,setTodos] = useState([]);
+
+  const addTodoHandler = (input) => {
+    const newTodo = {
+      id: Math.floor(Math.random() *1000),
+      text: input,
+      isCompeleted:false
+    }
+    setTodos([...todos,newTodo]);
+  }
+
   return ( 
     <div className={styles.container}>
-      <TodoForm/>
+      <TodoForm addTodoHandler={addTodoHandler}/>
       <TodoList/>
     </div>
    );
