c86b49f18de72a450649dd7fbef4fec9680fe132
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-18 13:16:55 +0430

    add styles for Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,8 @@
+import styles from './Todo.module.css';
+
 const Todo = ({todo}) => {
   return ( 
-    <div>
+    <div className={styles.todo}>
       <div>{todo.text}</div>
       <div>
         <button>Edit</button>
