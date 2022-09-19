8b1adc57c701aa9c7c0512a1e1341f0e5fb88365
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-09-19 09:52:52 +0430

    add styles for todo text

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ const Todo = ({ todo, onComplete, onDelete, onEdit }) => {
     <div className={styles.todo}>
       <div
         onClick={onComplete}
-        className={`${todo.isCompleted && styles.completed}`}
+        className={`${styles.todoText} ${todo.isCompleted && styles.completed}`}
       >
         {todo.text}
       </div>
